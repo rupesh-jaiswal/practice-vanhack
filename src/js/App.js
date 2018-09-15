5de4bbf3c81b585d31e6c80f6974d0a5552fb8e4
Rupesh Jaiswal <jaiswalrupesh2011@gmail.com>
2018-09-15 21:44:50 +0530

    added folder structure

--- src/js/App.js
+++ src/js/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import logo from '../img/logo.svg';
+import '../styles/App.scss';
 
 class App extends Component {
   render() {
